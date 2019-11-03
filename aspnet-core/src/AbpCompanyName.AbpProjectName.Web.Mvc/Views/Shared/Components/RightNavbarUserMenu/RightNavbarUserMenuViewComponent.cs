466b14b7b91d56ce715e4f05b7d6cfe243da7f64
iyilm4z <iyilm4z@gmail.com>
2019-11-04 02:16:27 +0300

    misc1

---
+++ aspnet-core/src/AbpCompanyName.AbpProjectName.Web.Mvc/Views/Shared/Components/RightNavbarUserMenu/RightNavbarUserMenuViewComponent.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AbpCompanyName.AbpProjectName.Sessions;
+using Abp.Configuration.Startup;
+using System.Threading.Tasks;
+
+namespace AbpCompanyName.AbpProjectName.Web.Views.Shared.Components.RightNavbarUserMenu
+{
+    public class RightNavbarUserMenuViewComponent : AbpProjectNameViewComponent
+    {
+        private readonly ISessionAppService _sessionAppService;
+        private readonly IMultiTenancyConfig _multiTenancyConfig;
+
+        public RightNavbarUserMenuViewComponent(ISessionAppService sessionAppService, 
+            IMultiTenancyConfig multiTenancyConfig)
+        {
+            _sessionAppService = sessionAppService;
+            _multiTenancyConfig = multiTenancyConfig;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var model = new RightNavbarUserMenuViewModel
+            {
+                LoginInformations = await _sessionAppService.GetCurrentLoginInformations(),
+                IsMultiTenancyEnabled = _multiTenancyConfig.IsEnabled,
+            };
+
+            return View(model);
+        }
+    }
+}
